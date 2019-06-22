6771e0e3b4c3b3adcf3a346cccb0f22bc26c965c
Paweł Pustelnik <pawel.pustelnik@exorigo-upos.pl>
2019-06-22 18:42:30 +0200

    rozwiązanie

--- blog.js
+++ blog.js
@@ -2,13 +2,13 @@ $(function () {
     $.get({
         // url: "http://www.mocky.io/v2/5d0da2bb3400005d00ca4a31"
         url: 'http://www.mocky.io/v2/5d0da8313400007c00ca4a38'
-
     }).done(function (data) {
-        console.log(data);
-    }).fail(function (e) {
-        alert("Wystąpił błąd w połączeniu");
-        console.log(e)
-    }).always(function () {
-            $('.loading').hide();
-        });
+        console.log(data)
+        data.forEach(article => {
+            $(".articles").append(`<article class="article">
+                    <header class="article__header">${article.title}</header>
+                <div class="article_content">${article.content}</div>
+                </article>`)
+        })
+    });
 })
